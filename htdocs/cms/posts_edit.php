76112c1085635a5534be9b610d1db30a933bd245
Giet22pl <127026496+Giet22pl@users.noreply.github.com>
2024-04-20 12:28:33 +0200

    poprawki

--- htdocs/cms/posts_edit.php
+++ htdocs/cms/posts_edit.php
@@ -7,71 +7,105 @@ secure();
 
 include('includes/header.php');
 
-if (isset($_POST['title'])){
+if (isset($_POST['title'])) {
 
-    if ($stm = $connect->prepare('INSERT INTO posts (title, content, author, date) VALUES (?, ?, ?, ?)')){
-        
-        $stm->bind_param('ssis', $_POST['title'], $_POST['content'], $_SESSION['id'],  $_POST['date']);
+    if ($stm = $connect->prepare('UPDATE posty set  tytul = ?, tresc = ? , data = ?  WHERE id = ?')) {
+        $stm->bind_param('sssi', $_POST['title'], $_POST['content'], $_POST['date'], $_GET['id']);
         $stm->execute();
-        
 
-        set_message("Nowy post dodany przez " . $_SESSION['username']);
-        header('Location: posts.php');
+
+
+
         $stm->close();
+
+        set_message("Post o id  " . $_GET['id'] . " zostal zaktualizowany");
+        header('Location: posts.php');
         die();
 
     } else {
-        echo 'Nie mo¿na przygotowaæ instrukcji!';
+        echo 'Could not prepare post update statement statement!';
     }
+
+
+
 
 
 }
 
 
-?>
-<div class="container mt-5">
-    <div class="row justify-content-center">
-        <div class="col-md-10">
-        <h1 class="display-1">Dodaj post</h1>
-       
-        <form method="post">
-                <!-- Username input -->
-                <div class="form-outline mb-4">
-                    <input type="text" id="title" name="title" class="form-control" />
-                    <label class="form-label" for="title">Tytu³</label>
+if (isset($_GET['id'])) {
+
+    if ($stm = $connect->prepare('SELECT * from posty WHERE id = ?')) {
+        $stm->bind_param('i', $_GET['id']);
+        $stm->execute();
+
+        $result = $stm->get_result();
+        $post = $result->fetch_assoc();
+
+        if ($post) {
+
+
+            ?>
+            <div class="container mt-5">
+                <div class="row justify-content-center">
+                    <div class="col-md-6">
+                        <h1 class="display-1">Edytuj post</h1>
+
+                        <form method="post">
+                            
+                            <div class="form-outline mb-4">
+                                <input type="text" id="title" name="title" class="form-control"
+                                    value="<?php echo $post['tytul'] ?>" />
+                                <label class="form-label" for="title">Tytul</label>
+                            </div>
+
+
+                            
+                            <div class="form-outline mb-4">
+                                <textarea name="content" id="content"><?php echo $post['tresc'] ?></textarea>
+                            </div>
+
+                            
+                            <div class="form-outline mb-4">
+                                <input type="date" id="date" name="date" class="form-control" value="<?php echo $post['data'] ?>" />
+                                <label class="form-label" for="date">Data</label>
+
+
+                            </div>
+
+                            
+                            <button type="submit" class="btn btn-primary btn-block">Edytuj post</button>
+                        </form>
+
+
+
+                    </div>
+
                 </div>
-     
-
-                <!-- Content input -->
-                <div class="form-outline mb-4">
-                    <textarea name="content" id="content" ></textarea>
-                </div>
-
-                <!-- Date select -->
-                <div class="form-outline mb-4">
-                <input type="date" id="date"  name="date" class="form-control" />
-                <label class="form-label" for="date">Data</label>
+            </div>
 
 
-                </div>
+            <script src="js/tinymce/tinymce.min.js"></script>
+            <script>
+                tinymce.init({
+                    selector: '#content'
+                });
+            </script>
 
-                <!-- Submit button -->
-                <button type="submit" class="btn btn-primary btn-block">Dodaj post</button>
-            </form>
+
+            <?php
+        }
+        $stm->close();
 
 
-       
-        </div>
+    } else {
+        echo 'Could not prepare statement!';
+    }
 
-    </div>
-</div>
+} else {
+    echo "Nie wybrano uzytkownika";
+    die();
+}
 
-<script src="js/tinymce/tinymce.min.js"></script>
-<script>
-    tinymce.init({
-        selector: '#content'
-    });
-</script>
-<?php
 include('includes/footer.php');
 ?>
